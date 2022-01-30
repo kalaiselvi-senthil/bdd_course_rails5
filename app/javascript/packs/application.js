3293b488e8d035a020fc8583a3ffb55326b2d8d4
kalaiselvi-senthil <kalai.datascientist@gmail.com>
2022-01-30 15:25:46 +0800

    Set up user-signup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,19 +2,29 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@popperjs/core")
+
+ require("@popperjs/core")
+ require("jquery")
+
+// require('popper.js')
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-
-
+//import jQuery from "jquery"
 import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+// var jQuery = require('jquery')
+
+// // include jQuery in global and window scope (so you can access it globally)
+// // in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+// global.$ = global.jQuery = jQuery;
+// window.$ = window.jQuery = jQuery;
 
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,11 @@ const webpack = require("webpack")
 
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
     $: 'jquery',
+    jquery: 'jquery',
     jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    // $: 'jquery/src/jquery',
+    // jQuery: 'jquery/src/jquery'
     Popper: ['popper.js', 'default']  // Not a typo, we're still using popper.js here
 }))
 // End new addition
